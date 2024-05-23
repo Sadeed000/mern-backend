b692ad973116e2f16ea256f07c3790b94bfa989f
Shadeed <134431222+Sadeed000@users.noreply.github.com>
2024-05-23 10:48:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ const fileupload = require('express-fileupload');
 
 app.use(fileupload({useTempFiles: true}))
 const corsOptions = {
-    origin: 'https://mern-frontend-o7nhmg37z-sadeed000s-projects.vercel.app/', // specify your client's origin
+    origin: '', // specify your client's origin
     credentials: true, // allow credentials (cookies, etc.)
   };
 app.use(cors({
-    origin: 'https://mern-frontend-o7nhmg37z-sadeed000s-projects.vercel.app/',
+    origin: '',
     credentials: true
   }));
   app.use(cors());
