f292f14a70e155db47cf9f16636bdd84c04a8ed9
Shadeed <134431222+Sadeed000@users.noreply.github.com>
2024-05-23 09:29:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,11 +21,11 @@ app.use(cors({
   app.use(cors());
 
 
-const corsOptions ={
-   origin:'', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
+// const corsOptions ={
+//    origin:'', 
+//    credentials:true,            //access-control-allow-credentials:true
+//    optionSuccessStatus:200,
+// }
 
   app.use(cors(corsOptions));
   
