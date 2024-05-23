a2e0fe43b9e665c7d0fd5cd7ed5cd3ae393bb9e5
Shadeed <134431222+Sadeed000@users.noreply.github.com>
2024-05-23 14:03:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,23 +11,34 @@ const fileupload = require('express-fileupload');
 
 app.use(fileupload({useTempFiles: true}))
 const corsOptions = {
-    origin: 'https://mern-frontend-nnqw.vercel.app', // Your frontend URL
-    credentials: true, // Allow credentials
-    optionsSuccessStatus: 200 // Some legacy browsers choke on 204
-};
-
-app.use(cors(corsOptions));
-
+    origin: 'https://mern-frontend-bzis.vercel.app/', // specify your client's origin
+    credentials: true, // allow credentials (cookies, etc.)
+  };
 app.use(cors({
-    origin: 'https://mern-frontend-nnqw.vercel.app',
+    origin: 'https://mern-frontend-bzis.vercel.app/',
     credentials: true
   }));
   app.use(cors());
 
 
-app.use(express.json()); // Parse JSON bodie
+// const corsOptions ={
+//    origin:'http://localhost:3000', 
+//    credentials:true,            //access-control-allow-credentials:true
+//    optionSuccessStatus:200,
+// }
 
+  app.use(cors(corsOptions));
+  
 
+// app.use(cors({
+//     origin : `http://localhost:4000`,
+//     credentials : true
+// }))  
+
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to ecommerce app</h1>");
+});
+app.use(express.json())
 app.use(cookieParser())
 
 app.use("/api",router)
