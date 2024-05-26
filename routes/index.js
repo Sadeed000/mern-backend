81e4d09a2a7580ff6145b68beb586f7f78895624
Shadeed <134431222+Sadeed000@users.noreply.github.com>
2024-05-26 17:48:40 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,65 @@
+// const express = require('express')
+
+// const router = express.Router()
+
+// const userSignUpController = require("../controller/user/userSignUp")
+// const userSignInController = require('../controller/user/userSignIn')
+// const userDetailsController = require('../controller/user/userDetails')
+// const authToken = require('../middleware/authToken')
+// const userLogout = require('../controller/user/userLogout')
+// const allUsers = require('../controller/user/allUsers')
+// const updateUser = require('../controller/user/updateUser')
+// const UploadProductController = require('../controller/product/uploadProduct')
+// const getProductController = require('../controller/product/getProduct')
+// const updateProductController = require('../controller/product/updateProduct')
+// const getCategoryProduct = require('../controller/product/getCategoryProductOne')
+// const getCategoryWiseProduct = require('../controller/product/getCategoryWiseProduct')
+// const getProductDetails = require('../controller/product/getProductDetails')
+// const addToCartController = require('../controller/user/addToCartController')
+// const countAddToCartProduct = require('../controller/user/countAddToCartProduct')
+// const addToCartViewProduct  = require('../controller/user/addToCartViewProduct')
+// const updateAddToCartProduct = require('../controller/user/updateAddToCartProduct')
+// const deleteAddToCartProduct = require('../controller/user/deleteAddToCartProduct')
+// const searchProduct = require('../controller/product/searchProduct')
+// const filterProductController = require('../controller/product/filterProduct')
+
+
+
+// router.post("/signup",userSignUpController)
+// router.post('/signin', userSignInController)
+// router.get("/user-details",authToken,userDetailsController)
+// router.get("/userLogout",userLogout)
+
+// //admin panel 
+
+// router.get("/all-user",allUsers)
+// router.post("/update-user/:id",updateUser)
+
+// //product
+// router.post("/upload-product",UploadProductController)
+// router.get("/get-product",getProductController)
+// router.post("/update-product",updateProductController)
+// router.get("/get-categoryProduct",getCategoryProduct)
+// router.post("/category-product",getCategoryWiseProduct)
+// router.post("/product-details",getProductDetails)
+// router.get("/search",searchProduct)
+// router.post("/filter-product",filterProductController)
+
+// //user add to cart
+// router.post("/addtocart",addToCartController)
+// router.get("/countAddToCartProduct",countAddToCartProduct)
+// router.get("/view-card-product",addToCartViewProduct)
+// router.post("/update-cart-product",updateAddToCartProduct)
+// router.post("/delete-cart-product",deleteAddToCartProduct)
+
+
+
+
+
+
+
+// module.exports = router
+
 const express = require('express')
 
 const router = express.Router()
@@ -26,19 +88,18 @@ const filterProductController = require('../controller/product/filterProduct')
 
 
 router.post("/signup",userSignUpController)
-router.post('/signin', userSignInController)
+router.post("/signin",userSignInController)
 router.get("/user-details",authToken,userDetailsController)
 router.get("/userLogout",userLogout)
 
 //admin panel 
-
-router.get("/all-user",allUsers)
-router.post("/update-user/:id",updateUser)
+router.get("/all-user",authToken,allUsers)
+router.post("/update-user",authToken,updateUser)
 
 //product
-router.post("/upload-product",UploadProductController)
+router.post("/upload-product",authToken,UploadProductController)
 router.get("/get-product",getProductController)
-router.post("/update-product",updateProductController)
+router.post("/update-product",authToken,updateProductController)
 router.get("/get-categoryProduct",getCategoryProduct)
 router.post("/category-product",getCategoryWiseProduct)
 router.post("/product-details",getProductDetails)
@@ -46,11 +107,11 @@ router.get("/search",searchProduct)
 router.post("/filter-product",filterProductController)
 
 //user add to cart
-router.post("/addtocart",addToCartController)
-router.get("/countAddToCartProduct",countAddToCartProduct)
-router.get("/view-card-product",addToCartViewProduct)
-router.post("/update-cart-product",updateAddToCartProduct)
-router.post("/delete-cart-product",deleteAddToCartProduct)
+router.post("/addtocart",authToken,addToCartController)
+router.get("/countAddToCartProduct",authToken,countAddToCartProduct)
+router.get("/view-card-product",authToken,addToCartViewProduct)
+router.post("/update-cart-product",authToken,updateAddToCartProduct)
+router.post("/delete-cart-product",authToken,deleteAddToCartProduct)
 
 
 
